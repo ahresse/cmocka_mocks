a9a601ea9631cf106880f92ff2a9bd052c5bcd87
Friedrich Schwedler <fschwedler@emlix.com>
2023-07-06 14:08:35 +0200

    fix formating
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- src/mock_openssl/include/cmocka_mocks/mock_openssl.h
+++ src/mock_openssl/include/cmocka_mocks/mock_openssl.h
@@ -21,12 +21,16 @@
 // clang-format on
 
 MOCK_FUNC_VAR_EXTERN(PEM_read_PUBKEY);
-EVP_PKEY *MOCK_FUNC_WRAP(PEM_read_PUBKEY)(FILE *fp, EVP_PKEY **x, pem_password_cb *cb, void *u);
-EVP_PKEY *MOCK_FUNC_REAL(PEM_read_PUBKEY)(FILE *fp, EVP_PKEY **x, pem_password_cb *cb, void *u);
+EVP_PKEY *MOCK_FUNC_WRAP(PEM_read_PUBKEY)(FILE *fp, EVP_PKEY **x,
+                                          pem_password_cb *cb, void *u);
+EVP_PKEY *MOCK_FUNC_REAL(PEM_read_PUBKEY)(FILE *fp, EVP_PKEY **x,
+                                          pem_password_cb *cb, void *u);
 
 MOCK_FUNC_VAR_EXTERN(EVP_DecodeBlock);
-int MOCK_FUNC_WRAP(EVP_DecodeBlock)(unsigned char *t, const unsigned char *f, int n);
-int MOCK_FUNC_REAL(EVP_DecodeBlock)(unsigned char *t, const unsigned char *f, int n);
+int MOCK_FUNC_WRAP(EVP_DecodeBlock)(unsigned char *t, const unsigned char *f,
+                                    int n);
+int MOCK_FUNC_REAL(EVP_DecodeBlock)(unsigned char *t, const unsigned char *f,
+                                    int n);
 
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
 MOCK_FUNC_VAR_EXTERN(EVP_MD_CTX_new);
@@ -43,18 +47,26 @@ const EVP_MD *MOCK_FUNC_WRAP(EVP_get_digestbyname)(const char *name);
 const EVP_MD *MOCK_FUNC_REAL(EVP_get_digestbyname)(const char *name);
 
 MOCK_FUNC_VAR_EXTERN(EVP_DigestVerifyInit);
-int MOCK_FUNC_WRAP(EVP_DigestVerifyInit)(EVP_MD_CTX *ctx, EVP_PKEY_CTX **pctx, const EVP_MD *type, ENGINE *e,
+int MOCK_FUNC_WRAP(EVP_DigestVerifyInit)(EVP_MD_CTX *ctx, EVP_PKEY_CTX **pctx,
+                                         const EVP_MD *type, ENGINE *e,
                                          EVP_PKEY *pkey);
-int MOCK_FUNC_REAL(EVP_DigestVerifyInit)(EVP_MD_CTX *ctx, EVP_PKEY_CTX **pctx, const EVP_MD *type, ENGINE *e,
+int MOCK_FUNC_REAL(EVP_DigestVerifyInit)(EVP_MD_CTX *ctx, EVP_PKEY_CTX **pctx,
+                                         const EVP_MD *type, ENGINE *e,
                                          EVP_PKEY *pkey);
 
 MOCK_FUNC_VAR_EXTERN(EVP_DigestUpdate);
-int MOCK_FUNC_WRAP(EVP_DigestUpdate)(EVP_MD_CTX *ctx, const void *d, size_t cnt);
-int MOCK_FUNC_REAL(EVP_DigestUpdate)(EVP_MD_CTX *ctx, const void *d, size_t cnt);
+int MOCK_FUNC_WRAP(EVP_DigestUpdate)(EVP_MD_CTX *ctx, const void *d,
+                                     size_t cnt);
+int MOCK_FUNC_REAL(EVP_DigestUpdate)(EVP_MD_CTX *ctx, const void *d,
+                                     size_t cnt);
 
 MOCK_FUNC_VAR_EXTERN(EVP_DigestVerifyFinal);
-int MOCK_FUNC_WRAP(EVP_DigestVerifyFinal)(EVP_MD_CTX *ctx, const unsigned char *sig, size_t siglen);
-int MOCK_FUNC_REAL(EVP_DigestVerifyFinal)(EVP_MD_CTX *ctx, const unsigned char *sig, size_t siglen);
+int MOCK_FUNC_WRAP(EVP_DigestVerifyFinal)(EVP_MD_CTX *ctx,
+                                          const unsigned char *sig,
+                                          size_t siglen);
+int MOCK_FUNC_REAL(EVP_DigestVerifyFinal)(EVP_MD_CTX *ctx,
+                                          const unsigned char *sig,
+                                          size_t siglen);
 
 MOCK_FUNC_VAR_EXTERN(EVP_PKEY_free);
 void MOCK_FUNC_WRAP(EVP_PKEY_free)(EVP_PKEY *pkey);
