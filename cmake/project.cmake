cf5bbb22f4f35750e14829e85e09adbd98615ffe
Friedrich Schwedler <fschwedler@emlix.com>
2023-08-31 15:12:47 +0200

    only add doc target if it doesn*t exist
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -87,6 +87,9 @@ endfunction()
 
 
 function(project_add_documentation_target)
+  if (TARGET cmocka_mocks_doc)
+    return()
+  endif ()
   cmake_parse_arguments(PARAM "" "MARKDOWN;TITLE" "" ${ARGN})
 
   find_program(PLANTUML NAMES plantuml)
